989edd0fbfb0873844dcf0c879017b98bb2f8352
margaret-hu <70117942+margaret-hu@users.noreply.github.com>
2023-05-02 23:45:28 -0700

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,43 +1,47 @@
 from tkinter import *
 from tkinter import messagebox
+import tkinter.font as font
 
 root = Tk()
+root.wm_title("Tic Tac Toe")
 board = []
 for r in range(3):
     row_list = []
     for c in range(3):
-        b = Button(root, text=" ", bg="ivory", width=5, command=lambda _r=r, _c=c: pick(_r, _c))
+        b = Button(root, text=" ", bg="ivory", width=5, height=3, command=lambda _r=r, _c=c: pick(_r, _c))
+        b['font'] = font.Font(size=20)
         row_list.append(b)
         b.grid(row=r, column=c)
     board.append(row_list)
 
 current = "x"
+
+
 def pick(r, c):
     global current
     button = board[r][c]
-    sel = button["text"]
-    if not (sel == " "):
-        print("already occupied by", sel, ": move ignored")
+    select = button["text"]
+    if not (select == " "):
         return
     button["text"] = current
     has_winner = check_win(r, c)
-    print("has_winner is", has_winner)
     if has_winner:
-        answer = messagebox.askyesno("need your input", current + " wins, new game?")
+        answer = messagebox.askyesno("Game Over", current + " wins, new game?")
         if answer == True:
             for r in range(3):
                 for c in range(3):
                     board[r][c]["text"] = " "
         else:
-            exit()        
+            exit()
     current = "o" if current == "x" else "x"
+
 
 def check_win(r, c):
     if board[r][0]["text"] == board[r][1]["text"] == board[r][2]["text"]:
         return True
     if board[0][c]["text"] == board[1][c]["text"] == board[2][c]["text"]:
         return True
-    a = board[1][1]["text"] # the diagonal
+    a = board[1][1]["text"]  # the diagonal
     if a == " ":
         return False
     if board[0][0]["text"] == a == board[2][2]["text"]:
@@ -45,5 +49,6 @@ def check_win(r, c):
     if board[0][2]["text"] == a == board[2][0]["text"]:
         return True
     return False
+
 
 root.mainloop()
